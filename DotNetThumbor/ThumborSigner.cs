d4348a7a31b35e8981db1a8c144bda83d0123535
Ben Ho <ben.ho@mi9.com.au>
2015-03-10 14:41:59 +1100

    Starting to add in the signed url's code.

---
+++ DotNetThumbor/ThumborSigner.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetThumbor
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class ThumborSigner
+    {
+
+        // base64.urlsafe_b64encode(hmac.new("355b08da1ed57db2ec8014032f68ec1a"[:16], unicode("https://s3-ap-southeast-2.amazonaws.com/mi9public/image_resizer_test/original/text.jpg").encode('utf-8'), hashlib.sha1).digest())
+        public string Encode(string input, byte[] key)
+        {
+            var hmacsha1 = new HMACSHA1(key);
+            var byteArray = Encoding.UTF8.GetBytes(input);
+            var stream = new MemoryStream(byteArray);
+            var tmp = hmacsha1.ComputeHash(stream);
+
+            return Convert.ToBase64String(tmp).Replace("+", "-").Replace("/", "_");
+        }
+    }
+}
---
+++ DotNetThumborTest/TestThumborSigner.cs
@@ -0,0 +1,35 @@
+﻿namespace DotNetThumborTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using DotNetThumbor;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestThumborSigner
+    {
+
+        [Test]
+        public void TestMethod1()
+        {
+            var key = Encoding.UTF8.GetBytes("");
+            var t = new ThumborSigner();
+
+            var list = new List<string>();
+
+            var input = "https://searchcode.com/static/searchcode_logo.png";
+
+            list.Add(input);
+            
+            var result = t.Encode(string.Join("/", list), key);
+
+            var url = string.Format("http://test/{0}/{1}", result, input);
+
+        }
+    }
+}
