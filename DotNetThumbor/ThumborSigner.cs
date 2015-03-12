51ed20677be65d45af1168531ca127eaea7d5ee1
Ben Ho <ben.ho@mi9.com.au>
2015-03-12 11:20:08 +1100

    Add in comments for ThumborSigner.

--- DotNetThumbor/ThumborSigner.cs
+++ DotNetThumbor/ThumborSigner.cs
@@ -7,6 +7,12 @@
 
     public class ThumborSigner : IThumborSigner
     {
+        /// <summary>
+        /// Method to sign Thumbor urls correct as of 2015/03/12
+        /// </summary>
+        /// <param name="input">The image URL that thumbor expects</param>
+        /// <param name="key">The thumbor secret key</param>
+        /// <returns>The signed result which can be passed to thumbor</returns>
         public string Encode(string input, string key)
         {
             var hmacsha1 = new HMACSHA1(Encoding.UTF8.GetBytes(key));
@@ -14,6 +20,7 @@
             var stream = new MemoryStream(byteArray);
             var tmp = hmacsha1.ComputeHash(stream);
 
+            // Thumbor implementation replaces + and / and so is replicated here
             return Convert.ToBase64String(tmp).Replace("+", "-").Replace("/", "_");
         }
     }
